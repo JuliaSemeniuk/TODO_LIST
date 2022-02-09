0bcc10f803a675a1cbcb9a80103cd16c02610ea4
julia semeniuk <jul.semeniuk@gmail.com>
2022-02-09 10:41:52 +0200

    implement adding functionality

--- JS_DOM/todo_list/index.js
+++ JS_DOM/todo_list/index.js
@@ -1,7 +1,44 @@
-const testConst = "test";
+const tasksFormInput = document.querySelector(".tasks__form-input");
+const tasksFormButton = document.querySelector(".tasks__form-button");
+const tasksContentList = document.querySelector(".tasks-content__list");
 
-const testFunc = (str) => {
-  return str;
-};
+console.dir(tasksFormInput);
+console.dir(tasksFormButton);
+console.dir(tasksContentList);
 
-testFunc(testConst);
+tasksFormButton.addEventListener("click", addTask); // ф-я виконується асинхронно, бо вона є колбеком і буде виконуватись після кліка
+
+function addTask(event) {
+  event.preventDefault(); //запобігає перезавантаженню сторінки по кліку
+  console.log("add task");
+
+  const tasksListItem = document.createElement("li");
+  tasksListItem.classList.add("tasks-list__item");
+
+  const itemText = document.createElement("p");
+  itemText.classList.add("item__text");
+  itemText.innerText = "innerText testing";
+
+  tasksListItem.appendChild(itemText);
+
+  const itemButtons = document.createElement("div");
+  itemButtons.classList.add("item__buttons");
+
+  tasksListItem.appendChild(itemButtons);
+
+  const buttonDone = document.createElement("button");
+  buttonDone.classList.add("item__button");
+  buttonDone.classList.add("button__done");
+  buttonDone.innerHTML = '<i class="far fa-check-circle"></i>';
+
+  itemButtons.appendChild(buttonDone);
+
+  const buttonDelete = document.createElement("button");
+  buttonDelete.classList.add("item__button");
+  buttonDelete.classList.add("button__delete");
+  buttonDelete.innerHTML = '<i class="fas fa-window-close"></i>';
+
+  itemButtons.appendChild(buttonDelete);
+
+  tasksContentList.appendChild(tasksListItem);
+}
