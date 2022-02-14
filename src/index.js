6ee101a4c309ef120826eadbeced216ebd73ee08
Ju <j.semeniuk.v@gmail.com>
2022-02-14 11:00:25 +0200

    change app to work with array

--- src/index.js
+++ src/index.js
@@ -6,60 +6,66 @@ const tasksFormInput = document.querySelector(".tasks__form-input");
 const tasksFormButton = document.querySelector(".tasks__form-button");
 const tasksContentList = document.querySelector(".tasks-content__list");
 
-console.dir(tasksFormInput);
-console.dir(tasksFormButton);
-console.dir(tasksContentList);
-
 tasksFormButton.addEventListener("click", addTask);
+
+let tasks = [];
+window.tasks = tasks;
+
+function render(tasks) {
+  tasksContentList.innerHTML = "";
+  tasksFormInput.value = "";
+
+  tasks.forEach((task) => {
+    const tasksListItem = document.createElement("li");
+    tasksListItem.classList.add("tasks-list__item");
+
+    const itemText = document.createElement("p");
+    itemText.classList.add("item__text");
+
+    itemText.innerText = task.value;
+
+    tasksListItem.appendChild(itemText);
+
+    const itemButtons = document.createElement("div");
+    itemButtons.classList.add("item__buttons");
+
+    tasksListItem.appendChild(itemButtons);
+    const buttonDelete = document.createElement("button");
+    buttonDelete.classList.add("item__button", "button__delete");
+    buttonDelete.innerHTML = '<i class="fas fa-window-close"></i>';
+
+    itemButtons.appendChild(buttonDelete);
+
+    buttonDelete.addEventListener("click", deleteTasksListItem(task));
+    tasksContentList.appendChild(tasksListItem);
+  });
+}
 
 function addTask(event) {
   event.preventDefault();
-  console.log("add task");
 
   if (tasksFormInput.value === "") {
     alert("Input is empty");
     return;
   }
 
-  const tasksListItem = document.createElement("li");
-  tasksListItem.classList.add("tasks-list__item");
-
-  const itemText = document.createElement("p");
-  itemText.classList.add("item__text");
-
-  itemText.innerText = tasksFormInput.value;
-
-  tasksListItem.appendChild(itemText);
-
-  const itemButtons = document.createElement("div");
-  itemButtons.classList.add("item__buttons");
-
-  tasksListItem.appendChild(itemButtons);
-
-  // const buttonDone = document.createElement("button");
-  // buttonDone.classList.add("item__button", "button__done");
-  // buttonDone.innerHTML = '<i class="far fa-check-circle"></i>';
-
-  // itemButtons.appendChild(buttonDone);
-
-  const buttonDelete = document.createElement("button");
-  buttonDelete.classList.add("item__button", "button__delete");
-  buttonDelete.innerHTML = '<i class="fas fa-window-close"></i>';
-
-  itemButtons.appendChild(buttonDelete);
-
-  buttonDelete.addEventListener("click", deleteTasksListItem(tasksListItem));
-
-  tasksContentList.appendChild(tasksListItem);
-
-  tasksFormInput.value = "";
+  tasks.push({ id: tasks.length + 1, value: tasksFormInput.value });
+  render(tasks);
 }
 
-function deleteTasksListItem(item) {
+function deleteTasksListItem(task) {
   return () => {
+    console.log("task: ", task);
     let isDelete = confirm("Are you sure?");
+
     if (isDelete) {
-      item.remove();
+      const index = tasks.findIndex((currentTask) => {
+        return currentTask.id === task.id;
+      });
+      console.log("index: ", index);
+      tasks.splice(index, 1);
+
+      render(tasks);
     }
   };
 }
